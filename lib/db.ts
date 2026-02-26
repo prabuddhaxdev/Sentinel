58f7823b6149ae634b535beae3b8b3003b78ce34
prabuddhaxdev <prabuddha1749@gmail.com>
2026-02-26 12:11:18 +0530

    Update db logic with global prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,22 +1,20 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
 
-const prismaClientSingleton = ()=>{
-    return new PrismaClient({ adapter });
-}
-
-declare const globalThis: {
-    prismaGlobal:ReturnType<typeof prismaClientSingleton>;
-}& typeof global
-
-const prisma = globalThis.prismaGlobal || prismaClientSingleton();
-
-if(process.env.NODE_ENV!== "production") globalThis.prismaGlobal = prisma;
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 
 export default prisma;
